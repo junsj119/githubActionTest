972f8a8ee2a34b6d27bbb2da769c6a0b86ce2c47
junsj119 <wjstjddud119@naver.com>
2022-07-06 11:04:11 +0900

    .

--- G.ATest/src/main/java/com/hanghae/G/ATest/Application.java
+++ G.ATest/src/main/java/com/hanghae/G/ATest/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		log.info("let's go !!!!!!!!!!!!!!");
+		log.info("let's go !!!!!!!!!!!!!");
 		SpringApplication.run(Application.class, args);
 	}
 }

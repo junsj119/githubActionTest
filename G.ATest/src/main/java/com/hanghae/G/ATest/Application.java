07215ccff27f75973bff5d0f206d2518e195295e
junsj119 <wjstjddud119@naver.com>
2022-07-06 11:01:52 +0900

    CI Test

--- G.ATest/src/main/java/com/hanghae/G/ATest/Application.java
+++ G.ATest/src/main/java/com/hanghae/G/ATest/Application.java
@@ -1,13 +1,15 @@
 package com.hanghae.G.ATest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
+		log.info("let's go !!!!!!!!!!!!!!");
 		SpringApplication.run(Application.class, args);
 	}
-
 }

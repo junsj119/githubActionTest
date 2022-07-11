119509741f405295f570edd153476350e2d8d33b
junsj119 <wjstjddud119@naver.com>
2022-07-11 16:38:49 +0900

    ㅈㅂㅈㅂㅈㅂ

--- G.ATest/scripts/health_check.sh
+++ G.ATest/scripts/health_check.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 # Crawl current connected port of WAS
-CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 # Toggle port Number
@@ -17,12 +17,12 @@ else
 fi
 
 
-echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
+echo "> Start health check of WAS at 'http://13.125.253.116:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
     echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/health)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://13.125.253.116${TARGET_PORT}/health)
 
     if [ ${RESPONSE_CODE} -eq 200 ]; then
         echo "> New WAS successfully running"

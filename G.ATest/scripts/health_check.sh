0434d51423f0430b0ef9e51306209271d9b71d6a
junsj119 <wjstjddud119@naver.com>
2022-07-11 22:36:45 +0900

    qqa

--- G.ATest/scripts/health_check.sh
+++ G.ATest/scripts/health_check.sh
@@ -23,7 +23,7 @@ for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
     echo "> #${RETRY_COUNT} trying..."
    # RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://54.180.96.247:${TARGET_PORT}/health)
-    RESPONSE_CODE=$(curl -o /dev/null -w "%{http_code}"  http://54.180.96.247:${TARGET_PORT}/health)
+    RESPONSE_CODE=$(curl -o /dev/ -s -w "%{http_code}"  http://54.180.96.247:${TARGET_PORT}/health)
 
     if [ ${RESPONSE_CODE} -eq 200 ]; then
         echo "> New WAS successfully running"
